0ab159e54a00e9f5da5c980b6bfaabe4718926b0
Andrew T.S. Bedgood <andrew.bedgood@ryansg.com>
2020-03-06 11:40:17 -0600

    Updated vm info script

--- VMGuestInfo.psm1
+++ VMGuestInfo.psm1
@@ -19,9 +19,8 @@
 function Get-VMInfo
 {
     param(
-        #Specifies the VMs to get information for
-        [string[]]
-        $VMs,
+        #Specifies the VMs to get information for - defaults to wildcard to return all
+        [string[]]$VMs = '*',
         [validateset('DiskTypes','HardDisks','HardwareVersion','ResourcePool',
             'DataStores','ConfiguredOS','IPv6',
             'VeeamBackupFolder','DatastoreIDs','*')]
@@ -30,31 +29,41 @@ function Get-VMInfo
     $DefaultProps = ('Name','VMID','RunningOS','PowerState','IPv4',
             'VMHost','VeeamJobName','LastSuccessfulBackup')
     
+    #If values exist in properties, prepends DefaultProps to additional properties & assigns to ReturnProps
     if($Properties)
     {
         $ReturnProps = $DefaultProps + ($Properties | Sort-Object -Unique)
+        #If ReturnProps includes a '*', assigns ReturnProps only * value
         switch($ReturnProps)
         {
             * {$ReturnProps = '*'}
         }
     }
+    #If no values in Properties, assigns only DefaultProps to ReturnProps
     else{$ReturnProps = $DefaultProps}
 
     #Gets VMs & returns all non-excluded objects
     Get-VM $VMs | Select-Object Name, PowerState, HardwareVersion, ID, VMHost, ResourcePool, Notes, DatastoreIdList,ExtensionData,Guest |
     ForEach-Object { 
+        #Expands Notes property & assigns to ExpandedNotes var for parsing
         $ExpandedNotes = (($_ | Select-Object -ExpandProperty Notes)  -split ', ' -replace "Veeam Backup:\s+" -replace '\[' -replace '\]')
 
+        #Gets all DataStore IDs for VM object
         $DatastoreIDs = ($_ | Select-Object -ExpandProperty DatastoreIdList)
+        #Gets all DataStores for VMObject
         $DataStores = $DatastoreIDs | ForEach-Object {
                 $DatID = $_
                 Get-Datastore | Where-Object {$_.ID -match $DatID}
             }
 
+        #Gets all HardDisks connected to VM
         $HardDiskObjects = (Get-HardDisk $_.Name)
+        #Gets storage format for all attached HardDisks
         $DiskTypes = $HardDiskObjects.StorageFormat
+        #Gets file names of all attached hard disks
         $HardDiskNames = $HardDiskObjects.FileName
 
+        #Parses Notes property for Veeam backup information
         $VeeamTime = (($ExpandedNotes | Select-String 'Time:') -replace 'Time: ')
         [string]$VeeamJobName = (($ExpandedNotes | Select-String 'Job name:') -replace 'Job name: ')
         [string]$VeeamBackupFolder = (($ExpandedNotes | Select-String 'Backup folder:') -replace 'Backup folder: ')
@@ -80,6 +89,7 @@ function Get-VMInfo
             IPv4 = $_.Guest.IPAddress | Where-Object {$_ -notmatch ':'}
         }
     
+        #Returns the object with only selected properties
         $ReturnObj | Select-Object $ReturnProps
     }
 }
