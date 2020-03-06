d610f8bc820d6dce926498624a49d649d0551298
Andrew T.S. Bedgood <andrew.bedgood@ryansg.com>
2020-03-06 11:30:41 -0600

    Added VMGuestInfo script.

---
+++ VMGuestInfo.psm1
@@ -0,0 +1,85 @@
+#Requires -Module VMware.VimAutomation.Core
+<#
+.SYNOPSIS
+    Gets information of specified VMs hosted on connected vcenter servers.
+
+.DESCRIPTION
+    Utilizes vmware.PowerCLI to return information of specified VM guests on connected host servers.
+    PowerCLI may encounter issues when run in PowerShell x64.
+    If issues are encountered, it is recommended loading this module in PowerShell x86.
+
+.EXAMPLE
+    Get-VMInfo VM1, TestVM
+
+    Returns information of VM guests VM1 and TestVM
+
+.LINK
+    https://code.vmware.com/docs/9638/cmdlet-reference
+#>
+function Get-VMInfo
+{
+    param(
+        #Specifies the VMs to get information for
+        [string[]]
+        $VMs,
+        [validateset('DiskTypes','HardDisks','HardwareVersion','ResourcePool',
+            'DataStores','ConfiguredOS','IPv6',
+            'VeeamBackupFolder','DatastoreIDs','*')]
+        [string[]]$Properties
+    )
+    $DefaultProps = ('Name','VMID','RunningOS','PowerState','IPv4',
+            'VMHost','VeeamJobName','LastSuccessfulBackup')
+    
+    if($Properties)
+    {
+        $ReturnProps = $DefaultProps + ($Properties | Sort-Object -Unique)
+        switch($ReturnProps)
+        {
+            * {$ReturnProps = '*'}
+        }
+    }
+    else{$ReturnProps = $DefaultProps}
+
+    #Gets VMs & returns all non-excluded objects
+    Get-VM $VMs | Select-Object Name, PowerState, HardwareVersion, ID, VMHost, ResourcePool, Notes, DatastoreIdList,ExtensionData,Guest |
+    ForEach-Object { 
+        $ExpandedNotes = (($_ | Select-Object -ExpandProperty Notes)  -split ', ' -replace "Veeam Backup:\s+" -replace '\[' -replace '\]')
+
+        $DatastoreIDs = ($_ | Select-Object -ExpandProperty DatastoreIdList)
+        $DataStores = $DatastoreIDs | ForEach-Object {
+                $DatID = $_
+                Get-Datastore | Where-Object {$_.ID -match $DatID}
+            }
+
+        $HardDiskObjects = (Get-HardDisk $_.Name)
+        $DiskTypes = $HardDiskObjects.StorageFormat
+        $HardDiskNames = $HardDiskObjects.FileName
+
+        $VeeamTime = (($ExpandedNotes | Select-String 'Time:') -replace 'Time: ')
+        [string]$VeeamJobName = (($ExpandedNotes | Select-String 'Job name:') -replace 'Job name: ')
+        [string]$VeeamBackupFolder = (($ExpandedNotes | Select-String 'Backup folder:') -replace 'Backup folder: ')
+
+        #Parses Notes data & creates a new PSObject that can be piped to additional cmds
+        $ReturnObj = New-Object psobject -Property @{
+            Name = $_.Name
+            PowerState = $_.PowerState
+            HardwareVersion = $_.HardwareVersion
+            VMID = $_.ID
+            VMHost = $_.VMHost
+            ResourcePool = $_.ResourcePool
+            VeeamJobName = $VeeamJobName
+            LastSuccessfulBackup = %{if($VeeamTime){[datetime]$VeeamTime} else{$null}}
+            VeeamBackupFolder = $VeeamBackupFolder
+            DatastoreIDs = $DatastoreIDs
+            Datastores = $DataStores
+            HardDisks = $HardDiskNames
+            DiskTypes = $DiskTypes
+            RunningOS = $_.Guest.OSFullName
+            ConfiguredOS = $_.ExtensionData.Config.GuestFullName
+            IPv6 = $_.Guest.IPAddress | Where-Object {$_ -match ':'}
+            IPv4 = $_.Guest.IPAddress | Where-Object {$_ -notmatch ':'}
+        }
+    
+        $ReturnObj | Select-Object $ReturnProps
+    }
+}
