ce384442183650fc53bf23069172df6bdb1bd5d3
Andrew T.S. Bedgood <andrew.bedgood@ryansg.com>
2020-12-04 12:08:23 -0600

    Added comments

--- LoggingTimestamp.psm1
+++ LoggingTimestamp.psm1
@@ -1,3 +1,7 @@
+<#
+.SYNOPSIS
+Writes a color-coded timestamp to console - useful for logging transcripts.
+#>
 function Get-Timestamp
 {
     [alias('Write-Timestamp','timestamp','logtime','log')]
@@ -8,28 +12,33 @@ function Get-Timestamp
         [string]$message
     )
 
+    # Defines an empty out string, default write color, and flips noNewLine to true for writing log message
     [string]$outString  = $null
     [string]$outColor   = 'White'
     [bool]$noNewLine    = $true
 
+    # Defines a standard start log message & ensures noNewLine is not flipped when log output is written
     if($logType -match '^start')
     {
         $outString  = "START LOG:`t$(Get-Date -Format 'MM/dd/yyyy hh:mm:ss tt')"
         $outColor   = 'White'
         $noNewLine  = $false
     }
+    # Defines a standard end log message & ensures noNewLine is not flipped when log output is written
     elseif($logType -match '^end')
     {
         $outString  = "END LOG:`t$(Get-Date -Format 'MM/dd/yyyy hh:mm:ss tt')"
         $outColor   = 'White'
         $noNewLine  = $false
     }
-
+    # For all other log types, defines the string prefix & output colors
     else{        
+        # Log timestamp prefixes use different number of tabs to keep text rows aligned properly
         if($logType -match '^error' -or $logType -eq 'change' -or $logType -eq 'notice')
         {$tagString = "`t$($logType.ToUpper() -replace '\s+'):`t`t"}
         else{$tagString = "`t$($logType.ToUpper()):`t"}
 
+        # Defines write-host foreground color values
         switch($logType)
         {
             'information'           {$outColor  = 'Magenta'}
@@ -41,6 +50,7 @@ function Get-Timestamp
             default         {$outColor = 'White'}
         }
 
+        # Defines the outString based on log type & appends the message if one was passed into cmd
         $outString = "$(Get-Date -Format 'hh:mm:ss tt')$tagString"
         if($message)
         {
@@ -49,11 +59,12 @@ function Get-Timestamp
         }
     }
 
+    # Defines the hash table for Write-Host cmd splatting
     $writeHostHash = @{
         Object          = $outString
         ForegroundColor = $outColor
         NoNewLine       = $noNewLine
     }
-    
+    # Writes timestamp message to console w/values defined in writeHostHash hashtable
     Write-Host @writeHostHash
 }
